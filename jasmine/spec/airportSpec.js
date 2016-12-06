9666a157a181b69608d3ccf845d14cb40a2799c8
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-06 13:50:02 +0000

    landed plane

--- jasmine/spec/airportSpec.js
+++ jasmine/spec/airportSpec.js
@@ -1,12 +1,18 @@
 describe("Airport", function() {
   var airport;
+  var plane;
 
   beforeEach(function() {
     airport = new Airport();
+    plane = jasmine.createSpy();
   });
 
   it("should be able to contain planes", function() {
     expect(airport.planes).toEqual([]);
   });
 
+  it("should land a plane", function() {
+    airport.land(plane);
+    expect(airport.planes).toContain(plane)
+  });
 });
