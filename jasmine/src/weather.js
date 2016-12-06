d522da5869b919a97fde09ffdd843112cb611291
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-06 15:53:19 +0000

    oneliner function description

--- jasmine/src/weather.js
+++ jasmine/src/weather.js
@@ -1,3 +1,5 @@
+// Has randomly generated condition - either sunny or stormy
+
 function Weather() {
   var weatherArray = ["sunny", "sunny", "sunny", "stormy"];
   var weatherNow = weatherArray[Math.floor(Math.random() * weatherArray.length)];
