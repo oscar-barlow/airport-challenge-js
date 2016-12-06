732bb96ad0a216b1538d85c6711d77ffa399804c
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-06 15:47:37 +0000

    no need to convert randomised weather condition to string

--- jasmine/src/weather.js
+++ jasmine/src/weather.js
@@ -1,5 +1,5 @@
 function Weather() {
   var weatherArray = ["sunny", "sunny", "sunny", "stormy"];
-  var weatherNow = weatherArray[Math.floor(Math.random() * weatherArray.length)].toString();
+  var weatherNow = weatherArray[Math.floor(Math.random() * weatherArray.length)];
   this.condition = weatherNow;
 }
