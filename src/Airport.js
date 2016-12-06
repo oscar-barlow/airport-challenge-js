498c4b57736b5d0579544c007aa91df15e3be3d2
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-06 12:22:10 +0000

    fresh start on airport challenge

--- src/Airport.js
+++
@@ -1,7 +0,0 @@
-function Airport() {
-  this.planes = [];
-}
-
-Airport.prototype.land = function(plane) {
-  this.planes.push(plane)
-}
--- src/airplane.js
+++
@@ -1,7 +0,0 @@
-function Airplane(){
-  this.isLanded = false;
-}
-
-Airplane.prototype.land = function() {
-  this.isLanded = true;
-}
