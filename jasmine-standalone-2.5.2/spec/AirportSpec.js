df5467772aff95eaaa22f0ee297b4abbbe59f09d
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-07 12:24:22 +0000

    new and imporved js airport

--- jasmine-standalone-2.5.2/spec/AirportSpec.js
+++ jasmine-standalone-2.5.2/spec/AirportSpec.js
@@ -18,9 +18,14 @@
        expect(airport.planes()).toEqual([plane]);
      });
 
-     it('can clear planes for takeoff', function(){
-       airport.clearForLanding(plane);
-       airport.clearForTakeOff(plane);
-       expect(airport.planes()).toEqual([]);
+     it('can check for stormy conditions', function(){
+       expect(airport.isStormy()).toBeFalsy();
+     });
+
+     describe('Under stormy conditions', function(){
+       it('does not clear for takeoff', function(){
+         spyOn(airport, 'isStormy').and.returnValue(true);
+         expect(function(){ airport.clearForTakeOff(plane);}).toThrowError('cannot takeoff during storm');
+       });
      });
  });
